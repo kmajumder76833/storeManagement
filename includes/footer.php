8a0a1eafd6c50707641cb482c15a2d135dba7078
kmajumder76833 <kmajumder768@gmail.com>
2017-12-21 00:30:56 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</div> 
+	
+	<script src="assests/plugins/fileinput/js/plugins/canvas-to-blob.min.js'); ?>" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/sortable.min.js" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/purify.min.js" type="text/javascript"></script>
+	<script src="assests/plugins/fileinput/js/fileinput.min.js"></script>	
+
+
+	<script src="assests/plugins/datatables/jquery.dataTables.min.js"></script>
+
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once 'php_action/core.php';
+
+
+session_unset(); 
+
+ 
+session_destroy(); 
+
+header('location: http://localhost/stock/index.php');
+
+?>
